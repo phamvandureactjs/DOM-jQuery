ed88a0c48e666c85af2eb40ab23e8bad931b653a
phamvandureactjs <phamvandu.uet.vnu@gmail.com>
2023-01-13 12:52:29 +0700

    BT2

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculate(){
+    var SoTienVay = parseFloat(document.getElementById("SoTienVay").value);
+    var SoThang = (parseFloat(document.getElementById("ThoiGianTra").value)*12);    
+    var LaiSuatTheoThang = ((parseFloat(document.getElementById("LaiSuatHangNam").value))/1200).toFixed(4);
+
+    var x = (Math.pow(1 + LaiSuatTheoThang, SoThang)).toFixed(1);
+    var SoTienTraHangThang = ((SoTienVay*x*LaiSuatTheoThang)/(x-1)).toFixed(1);
+    var TongSoTienPhaiTra = SoTienTraHangThang*SoThang;
+    var TongSoLaiPhaiTra  = TongSoTienPhaiTra-SoTienVay;
+    // var SoTien1Thang = (SoTienVay/12).toFixed(1);
+    // var TongTien = SoThang * 30;
+    // var X = (Math.pow(1+LaiSuatTheoThang, SoThang));
+    var result1 = document.getElementById("SoTienTraHangThang");
+    var result2 = document.getElementById("TongSoTienPhaiTra");
+    var result3 = document.getElementById("TongSoLaiPhaiTra");
+
+    result1.textContent = SoTienTraHangThang;
+    result2.textContent = TongSoTienPhaiTra;
+    result3.textContent = TongSoLaiPhaiTra;
+}
